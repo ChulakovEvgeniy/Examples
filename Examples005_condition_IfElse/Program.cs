276d36e1fbc0490d27162f4919c2f6db1494f011
«ChulakovEvgeniy» <>
2023-03-26 00:04:28 +0300

    add Examples condition If Else

---
+++ Examples005_condition_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша") // добавка ToLover() позволяет автоматически устранить ошибку в ведение имени маша разного регистра букв
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
