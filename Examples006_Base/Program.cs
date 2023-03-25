a801d51c3fdec72e256998df66e4b5785437d817
«ChulakovEvgeniy» <>
2023-03-26 00:18:58 +0300

    add Examples maximum

---
+++ Examples006_Base/Program.cs
@@ -0,0 +1,18 @@
+﻿int a = 105;
+int b = 100;
+int c = 6;
+int d = 8;
+int e = 4;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("Max = ");
+Console.WriteLine(max);
+
+// нахождение максимального
