194e13eb32b86a732ca346e1424b1e473c848d36
«ChulakovEvgeniy» <>
2023-03-27 15:31:40 +0300

    examples 010 Method  array

---
+++ Examples010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {1, 12, 31, 24, 55, 36, 72, 18, 9};
+
+int n = array.Length;
+int find = 24;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine($"index данного числа = {index}");
+        break;
+    }
+
+    index++;
+}
